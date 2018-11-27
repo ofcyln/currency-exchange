99f51140bb7ef0efd897d33549cd4ae0de45a30c
Fikret Ceylan <ofcyln@gmail.com>
2018-11-27 23:08:38 +0300

    current date and time method optimizations for locale format

--- src/app/shared/service/currency-exchange.service.ts
+++ src/app/shared/service/currency-exchange.service.ts
@@ -30,7 +30,9 @@ export class CurrencyExchangeService implements OnInit {
         let currentMonth = new Date().getMonth() + 1;
         let currentYear = new Date().getFullYear();
 
-        this.currentDate = `${currentDay}/${currentMonth}/${currentYear}`;
+        this.currentDate = `${currentDay < 10 ? '0' : ''}${currentDay}/${
+            currentMonth < 10 ? '0' : ''
+        }${currentMonth}/${currentYear}`;
 
         return this.currentDate;
     }
@@ -40,7 +42,9 @@ export class CurrencyExchangeService implements OnInit {
         let currentMinute = new Date().getMinutes();
         let currentSecond = new Date().getSeconds();
 
-        this.currentTime = `${currentHour}:${currentMinute}:${currentSecond}`;
+        this.currentTime = `${currentHour < 10 ? '0' : ''}${currentHour}:${
+            currentMinute < 10 ? '0' : ''
+        }${currentMinute}:${currentSecond < 10 ? '0' : ''}${currentSecond}`;
 
         return this.currentTime;
     }
