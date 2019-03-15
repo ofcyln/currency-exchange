739ca5f72e96b9ccd8e09790ff21aedabb6d7257
Osman Fikret Ceylan <ofcyln@gmail.com>
2019-03-15 11:39:24 +0100

    added app component test file

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,28 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { AppModule } from './app.module';
+import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
+
+describe('AppComponent', () => {
+    let component: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [RouterTestingModule, AppModule, CoreModule, LoadingBarHttpClientModule],
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create the app', () => {
+        expect(component).toBeTruthy();
+    });
+});
