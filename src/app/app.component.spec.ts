859880094f40f85346a94d6914f5c29b9981ed09
Osman Fikret Ceylan <ofcyln@gmail.com>
2019-03-15 12:11:29 +0100

    added test to the app component test file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,51 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { AppModule } from './app.module';
-import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
+import { LoginComponent } from './auth/login/login.component';
+import { ConverterComponent } from './components/converter/converter.component';
+import { AuthGuardService } from './auth/auth-guard.service';
+import { HistoryComponent } from './components/history/history.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { APP_BASE_HREF } from '@angular/common';
 
 describe('AppComponent', () => {
     let component: AppComponent;
     let fixture: ComponentFixture<AppComponent>;
 
+    const appRoutes: Routes = [
+        {
+            path: '',
+            redirectTo: '/converter',
+            pathMatch: 'full',
+        },
+        { path: 'login', component: LoginComponent },
+        { path: 'converter', component: ConverterComponent, canActivate: [AuthGuardService] },
+        { path: 'history', component: HistoryComponent, canActivate: [AuthGuardService] },
+        { path: 'not-found', component: NotFoundComponent },
+        { path: '**', redirectTo: 'not-found' },
+    ];
+
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            imports: [RouterTestingModule, AppModule, CoreModule, LoadingBarHttpClientModule],
+            imports: [
+                RouterTestingModule,
+                AppModule,
+                CoreModule,
+                LoadingBarHttpClientModule,
+                RouterModule.forRoot(appRoutes),
+            ],
+            providers: [
+                {
+                    provide: APP_BASE_HREF,
+                    useValue: '/',
+                },
+            ],
         }).compileComponents();
     }));
 
@@ -25,4 +58,12 @@ describe('AppComponent', () => {
     it('should create the app', () => {
         expect(component).toBeTruthy();
     });
+
+    it('should check the `main` content has the class more than 2', async(() => {
+        const testbed = TestBed.createComponent(AppComponent);
+        testbed.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+
+        expect(compiled.querySelector('main').classList.length).toBeGreaterThan(2);
+    }));
 });
