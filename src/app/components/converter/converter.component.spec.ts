1f87ddc4ff969f2c5c21fe27403ed3285d763acb
Osman Fikret Ceylan <ofcyln@gmail.com>
2019-03-15 14:15:55 +0100

    added unit tests to converter component

--- src/app/components/converter/converter.component.spec.ts
+++ src/app/components/converter/converter.component.spec.ts
@@ -1,5 +1,5 @@
 import { DebugElement } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
 
 import { ConverterComponent } from './converter.component';
 import { BrowserModule, By } from '@angular/platform-browser';
@@ -22,12 +22,30 @@ import { HistoryComponent } from '../history/history.component';
 import { CurrencyExchangeService } from '../../shared/service/currency-exchange.service';
 import { ExchangeRatesApiRequestService } from '../../shared/service/exchange-rates-api-request.service';
 import { StorageService } from '../../shared/service/storage.service';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from '../../auth/login/login.component';
+import { AuthGuardService } from '../../auth/auth-guard.service';
+import { NotFoundComponent } from '../../core/not-found/not-found.component';
+import { APP_BASE_HREF } from '@angular/common';
 
 describe('ConverterComponent', () => {
     let compiled: ConverterComponent;
     let fixture: ComponentFixture<ConverterComponent>;
     let dElement: DebugElement;
     let hElement: HTMLElement;
+
+    const appRoutes: Routes = [
+        {
+            path: '',
+            redirectTo: '/converter',
+            pathMatch: 'full',
+        },
+        { path: 'login', component: LoginComponent },
+        { path: 'converter', component: ConverterComponent, canActivate: [AuthGuardService] },
+        { path: 'history', component: HistoryComponent, canActivate: [AuthGuardService] },
+        { path: 'not-found', component: NotFoundComponent },
+        { path: '**', redirectTo: 'not-found' },
+    ];
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -48,8 +66,17 @@ describe('ConverterComponent', () => {
                 MatSelectModule,
                 MatTableModule,
                 MatAutocompleteModule,
+                RouterModule.forRoot(appRoutes),
             ],
-            providers: [CurrencyExchangeService, ExchangeRatesApiRequestService, StorageService],
+            providers: [
+                CurrencyExchangeService,
+                ExchangeRatesApiRequestService,
+                StorageService,
+                {
+                    provide: APP_BASE_HREF,
+                    useValue: '/converter',
+                },
+            ],
         })
             .compileComponents()
             .then(() => {
@@ -76,5 +103,27 @@ describe('ConverterComponent', () => {
         hElement.click();
 
         expect(compiled.exchangeRates).toHaveBeenCalledTimes(0);
+    }));
+
+    it('form should be invalid while input fields are empty', async(() => {
+        fixture.detectChanges();
+
+        compiled.converterForm.controls['amountControl'].setValue('');
+        compiled.converterForm.controls['fromControl'].setValue('');
+        compiled.converterForm.controls['toControl'].setValue('');
+
+        expect(compiled.converterForm.valid).toBeFalsy();
+    }));
+
+    it('form should be valid while input fields are filled', async(() => {
+        fixture.detectChanges();
+
+        fakeAsync(() => {
+            compiled.converterForm.controls['amountControl'].setValue(1);
+            compiled.converterForm.controls['fromControl'].setValue('EUR');
+            compiled.converterForm.controls['toControl'].setValue('TRY');
+        });
+
+        expect(compiled.converterForm.valid).toBeFalsy();
     }));
 });
