0d978b0ea183096dfa5f34fecf1d599d7efbcfa8
Osman Fikret Ceylan <ofcyln@gmail.com>
2020-07-24 16:14:56 +0200

    PWA installation banner removed, Statistics section ordered as first in mobile view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { AuthService } from './auth/auth.service';
-import { StorageService } from './shared/service/storage.service';
 
 @Component({
     selector: 'app-root',
@@ -9,75 +8,7 @@ import { StorageService } from './shared/service/storage.service';
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-    public isBannerShown: string = 'false';
-    public showBanner: boolean = true;
-    public deferredPrompt: any;
+    constructor(public authService: AuthService) {}
 
-    constructor(public authService: AuthService, public element: ElementRef, public renderer: Renderer2) {}
-
-    ngOnInit(): void {
-        this.assignBannerShown();
-
-        this.beforeInstallPromt();
-
-        if (this.isChrome()) {
-            StorageService.setItem('isBannerShown', 'true');
-
-            this.assignBannerShown();
-        }
-
-        this.showIosBanner();
-    }
-
-    beforeInstallPromt() {
-        this.renderer.listen('window', 'beforeinstallprompt', (event) => {
-            event.preventDefault();
-
-            this.deferredPrompt = event;
-
-            this.addToHomeScreen();
-        });
-    }
-
-    assignBannerShown() {
-        this.isBannerShown = StorageService.getItem('isBannerShown') || 'false';
-    }
-
-    isIos() {
-        return /iphone|ipad|ipod/.test(window.navigator.userAgent.toLowerCase());
-    }
-
-    isChrome() {
-        return /google inc./.test(window.navigator.vendor.toLowerCase());
-    }
-
-    isInStandaloneMode() {
-        return 'standalone' in (window as any).navigator && (window as any).navigator.standalone;
-    }
-
-    showIosBanner() {
-        if (
-            this.isIos() &&
-            !this.isInStandaloneMode() &&
-            (this.isBannerShown === null || this.isBannerShown === 'false')
-        ) {
-            StorageService.setItem('isBannerShown', 'true');
-        }
-    }
-
-    addToHomeScreen() {
-        this.deferredPrompt.prompt();
-
-        this.deferredPrompt.userChoice.then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
-                StorageService.setItem('isBannerShown', 'true');
-            }
-
-            this.deferredPrompt = null;
-        });
-    }
-
-    close() {
-        this.showBanner = !this.showBanner;
-    }
+    ngOnInit(): void {}
 }
